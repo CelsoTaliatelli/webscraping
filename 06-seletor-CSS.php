a0d45df0f5d63cf9104263440773da81ca81319a
Celso Taliatelli <celsotaliatelli@hotmail.com>
2020-08-30 15:43:46 -0300

    seletores CSS

---
+++ 06-seletor-CSS.php
@@ -0,0 +1,13 @@
+<?php
+ 
+ use Symfony\Component\BrowserKit\HttpBrowser;
+ use Symfony\Component\HttpClient\HttpClient;
+
+ require_once 'vendor/autoload.php';
+
+ $browser = new HttpBrowser(HttpClient::create());
+
+
+$crawler = $browser->request('GET','https://vitormattos.github.io/poc-lineageos-cellphone-list-statics//');
+$text = $crawler->filter('.title-page')->text();
+var_dump($text);
