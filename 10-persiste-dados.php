17f82960386d353afc7d91d64da1cca75fb4ff29
Celso Taliatelli <celsotaliatelli@hotmail.com>
2020-09-01 21:59:46 -0300

    persistencia de dados

---
+++ 10-persiste-dados.php
@@ -0,0 +1,27 @@
+<?php
+ 
+ use Symfony\Component\BrowserKit\HttpBrowser;
+ use Symfony\Component\HttpClient\HttpClient;
+
+ require_once 'vendor/autoload.php';
+
+ $browser = new HttpBrowser(HttpClient::create());
+
+
+$crawler = $browser->request('GET','https://vitormattos.github.io/poc-lineageos-cellphone-list-statics//');
+$totalPaginas = $crawler->filter('header')->text();
+$totalPaginas = preg_replace('/\D/','',$totalPaginas);
+$totalPaginas = ceil($totalPaginas/10);
+   $modelos = $crawler->filter('article')->each(function($node){
+      $return['modelo'] = $node->filter('.title')->text();
+      $atributos = $node->filter('th')->each(function($attr){
+         return $attr->text();
+      });
+      $values = $node->filter('td')->each(function($val){
+         return $val->text();
+      });
+      $return = array_merge($return,array_combine($atributos,$values));
+         return $return;
+   });
+
+print_r($modelos);
